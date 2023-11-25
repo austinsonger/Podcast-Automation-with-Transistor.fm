13b4d14e590c6b1563ab6866ab20d38a62870fad
Austin Songer <a.songer@protonmail.com>
2023-11-25 13:37:00 -0600

    Update PodcastCover.py

--- PodcastCover.py
+++ PodcastCover.py
@@ -1,3 +1,30 @@
+"""
+# Image Text Overlay Script
+
+## Overview
+This Python script is designed to automate the process of overlaying text onto a predefined image template. It reads titles from a CSV file and places them onto an image, creating a series of new images with the text embedded. This script is particularly useful for generating customized images in bulk, such as podcast covers, event posters, or social media graphics.
+
+## Functionality
+- **CSV Reading**: The script reads data from a specified CSV file. Each row in the CSV should contain a title in the 'Title' column.
+- **Image Processing**: The script uses a predefined image as a template and overlays text onto this image.
+- **Text Placement and Formatting**: Text from the CSV file is placed at specified coordinates on the image. The font style, size, and color can be customized.
+- **Manual Text Wrapping**: The script expects that the text in the CSV file is pre-formatted with line breaks for proper text wrapping on the image.
+- **Image Output**: Each row from the CSV results in a new image file, saved with a name based on the corresponding title.
+
+## Requirements
+- Python 3
+- Pandas Library
+- Pillow (PIL) Library
+
+## Setup and Usage
+1. **Install Required Libraries**: Ensure Pandas and Pillow are installed in your Python environment.
+2. **CSV File**: Prepare your CSV file with titles. Ensure each title is appropriately formatted with line breaks (`\n`) where needed.
+3. **Image and Font Setup**: Place your image template and desired font file in the script's directory or specify their paths in the script.
+4. **Customization**: Adjust the script to set the font size and the coordinates for text placement on the image.
+5. **Execution**: Run the script. It will process each title from your CSV file and create new images in the specified output directory.
+
+"""
+
 import pandas as pd
 import os
 import textwrap

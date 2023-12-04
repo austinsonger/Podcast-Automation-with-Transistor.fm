bd0e59dba9ad34f8e4add65c01beba9cd5d03b18
Austin Songer <a.songer@protonmail.com>
2023-12-04 15:32:22 -0600

    Update download-scotus.py

--- src/download-scotus.py
+++ src/download-scotus.py
@@ -7,8 +7,7 @@ file_path = './config/scotus.csv'  # Replace with your actual file path
 scotus_df = pd.read_csv(file_path)
 
 # Base directory for the year
-# base_dir = '../2023/'
-base_dir = os.path.abspath('../2023/')
+base_dir = '../2023/'
 
 # Iterate over each row to generate and execute the curl commands
 for index, row in scotus_df.iterrows():
@@ -18,7 +17,8 @@ for index, row in scotus_df.iterrows():
     case_dir = os.path.join(base_dir, case_id)
 
     # Create directory for the case if it doesn't exist
-    os.makedirs(case_dir, exist_ok=True)
+    audio_dir = os.path.join(case_dir, 'audio')
+    os.makedirs(audio_dir, exist_ok=True)
 
     # Generate the curl command
     curl_command = f"curl https://www.supremecourt.gov/media/audio/mp3files/{case_id}.mp3 --output {case_dir}/audio/{case_id}.mp3"

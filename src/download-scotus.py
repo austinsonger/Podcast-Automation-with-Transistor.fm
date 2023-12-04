618b4742cdf0b9db75c50df43fa824e840a090a5
Austin Songer <a.songer@protonmail.com>
2023-12-04 15:17:42 -0600

    Update download-scotus.py

--- src/download-scotus.py
+++ src/download-scotus.py
@@ -7,7 +7,8 @@ file_path = './config/scotus.csv'  # Replace with your actual file path
 scotus_df = pd.read_csv(file_path)
 
 # Base directory for the year
-base_dir = '../2023/'
+# base_dir = '../2023/'
+base_dir = os.path.abspath('../2023/')
 
 # Iterate over each row to generate and execute the curl commands
 for index, row in scotus_df.iterrows():

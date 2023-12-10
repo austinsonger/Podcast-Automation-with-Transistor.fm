bf3b6e2d3e816d5aa3d41f73c37da387b53b3696
Austin Songer <a.songer@protonmail.com>
2023-12-09 23:56:59 -0600

    Update draft-scheduling-episodes.py

--- src/draft-scheduling-episodes.py
+++ src/draft-scheduling-episodes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from datetime import datetime
 import pytz
-import os  # Ensure os is imported for os.getenv
+import os
 
 # Constants
 CSV_FILE_PATH = './config/scotus.csv'
@@ -10,10 +10,14 @@ TRANSISTOR_API_KEY = os.getenv('TRANSISTOR_API_KEY')
 TRANSISTOR_SHOW_ID = '12890'
 TRANSISTOR_API_URL = 'https://api.transistor.fm/v1/episodes'
 
+# Paths for audio and images
+AUDIO_PATH = '../2023/audio/'
+IMAGES_PATH = '../2023/images/'
+
 # Headers for API requests
 headers = {"x-api-key": TRANSISTOR_API_KEY}
 
-def create_and_schedule_episode(title, description, audio_path, cover_image_path, argument_date):
+def create_and_schedule_episode(title, description, audio_filename, cover_image_filename, argument_date):
     cst_timezone = pytz.timezone('America/Chicago')
     scheduled_time = datetime.strptime(argument_date, '%Y-%m-%d').replace(tzinfo=pytz.utc).astimezone(cst_timezone)
     scheduled_time = scheduled_time.replace(hour=20, minute=0, second=0)  # 8 PM CST
@@ -33,11 +37,12 @@ def create_and_schedule_episode(title, description, audio_path, cover_image_path
         episode_id = episode_data['data']['id']
 
         # Upload audio file
-        # Commented out to disable audio uploading
-        # with open(audio_path, 'rb') as audio_file:
-        #     requests.post(f'{TRANSISTOR_API_URL}/{episode_id}/audio', headers=headers, files={'audio': audio_file})
+        audio_file_path = os.path.join(AUDIO_PATH, audio_filename)
+        with open(audio_file_path, 'rb') as audio_file:
+            requests.post(f'{TRANSISTOR_API_URL}/{episode_id}/audio', headers=headers, files={'audio': audio_file})
 
         # Upload cover image
+        cover_image_path = os.path.join(IMAGES_PATH, cover_image_filename)
         with open(cover_image_path, 'rb') as image_file:
             requests.post(f'{TRANSISTOR_API_URL}/{episode_id}/images', headers=headers, files={'image': image_file})
     else:
@@ -49,11 +54,11 @@ def main():
     for index, row in df.iterrows():
         title = row['title']
         description = row['description']
-        audio_path = row['audio_file_path']
-        cover_image_path = row['cover_image_path']
+        audio_filename = row['audio_file_path']
+        cover_image_filename = row['cover_image_path']
         argument_date = row['argument_date']
 
-        create_and_schedule_episode(title, description, audio_path, cover_image_path, argument_date)
+        create_and_schedule_episode(title, description, audio_filename, cover_image_filename, argument_date)
 
 if __name__ == "__main__":
     main()

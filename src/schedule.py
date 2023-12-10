7080a7e4ae510ead5a7a0547788542edcc97e4be
Austin Songer <a.songer@protonmail.com>
2023-12-10 01:01:56 -0600

    Create schedule.py

---
+++ src/schedule.py
@@ -0,0 +1,58 @@
+import csv
+import requests
+import os
+import re
+
+# Constants
+csv_file_path = './config/scotus.csv'
+api_url = "https://api.transistor.fm/v1/episodes" # Transistor.fm API endpoint and authorization
+transistor_api_key = os.getenv('TRANSISTOR_API_KEY')  # Transistor.fm API endpoint and authorization
+show_id = "12890"  # Transistor.fm API endpoint and authorization
+
+# Function to parse the CSV file and create draft episodes
+def create_draft_episodes_from_csv(csv_file_path, show_id):
+    episodes = []
+    with open(csv_file_path, mode='r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip the header row
+        for row in csv_reader:
+            if len(row) < 2:  # Ensure the row has at least title and summary
+                continue
+            title, summary = row[0], row[1]
+
+            # Extract the argument date from the title using regular expressions
+            argument_date_match = re.search(r'\[Arg:\s(\d+\.\d+\.\d+)\]', title)
+            argument_date = argument_date_match.group(1) if argument_date_match else None
+
+            episode = {
+                "episode[show_id]": show_id,
+                "episode[title]": title.strip(),
+                "episode[summary]": summary.strip(),
+                "episode[publish_at]": argument_date  # Set the argument date as the publish date
+            }
+            episodes.append(episode)
+    return episodes
+
+
+# Headers for the API request
+headers = {
+    "x-api-key": transistor_api_key
+}
+
+# Function to create a draft episode in Transistor
+def create_draft_episode_in_transistor(episode):
+    response = requests.post(api_url, data=episode, headers=headers)
+    return response
+
+# Main function to process the CSV and create episodes in Transistor
+def process_episodes(csv_file_path):
+    episodes = create_draft_episodes_from_csv(csv_file_path, show_id)
+    for episode in episodes:
+        response = create_draft_episode_in_transistor(episode)
+        if response.status_code == 201:
+            print(f"Draft episode '{episode['episode[title]']}' created successfully.")
+        else:
+            print(f"Failed to create episode '{episode['episode[title]']}'. Status Code: {response.status_code}, Response: {response.json()}")
+
+
+process_episodes(csv_file_path)

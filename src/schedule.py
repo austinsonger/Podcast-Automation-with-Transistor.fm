d28ff2414db2be1f5784b294afa6330df389acce
Austin Songer <a.songer@protonmail.com>
2023-12-10 02:48:35 -0600

    Update schedule.py

--- src/schedule.py
+++ src/schedule.py
@@ -2,7 +2,7 @@ import csv
 import requests
 import os
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Constants
 csv_file_path = './config/scotus.csv'
@@ -21,24 +21,10 @@ def create_draft_episodes_from_csv(csv_file_path, show_id):
                 continue
             title, summary = row[0], row[1]
 
-            # Extract the argument date from the title
-            argument_date_match = re.search(r'\[Arg:\s(\d+\.\d+\.\d+)\]', title)
-            if argument_date_match:
-                argument_date_str = argument_date_match.group(1)
-                # Adjust the date format to match the input format (MM.DD.YYYY)
-                argument_date = datetime.strptime(argument_date_str, '%m.%d.%Y')
-                # Add one day to account for UTC time zone
-                argument_date += timedelta(days=1)
-                # Format as ISO 8601 for 2:00 AM UTC on the next day
-                published_date = argument_date.strftime('%Y-%m-%dT02:00Z')
-            else:
-                published_date = None
-
             episode = {
                 "episode[show_id]": show_id,
                 "episode[title]": title.strip(),
-                "episode[summary]": summary.strip(),
-                "episode[published_at]": published_date
+                "episode[summary]": summary.strip()
             }
             episodes.append(episode)
     return episodes
@@ -54,16 +40,46 @@ def create_draft_episode_in_transistor(episode_data):
     response = requests.patch(endpoint, data=episode_data, headers=headers)
     return response
 
-# Main function to process the CSV and create episodes in Transistor
+# Function to update the episode with the publish date
+def update_episode_publish_date(episode_id, published_date):
+    endpoint = f"{api_url}/{episode_id}"
+    episode_data = {
+        "episode[published_at]": published_date
+    }
+    response = requests.patch(endpoint, data=episode_data, headers=headers)
+    return response
+
+# Main function to process the CSV and create/update episodes in Transistor
 def process_episodes(csv_file_path):
     episodes = create_draft_episodes_from_csv(csv_file_path, show_id)
     for episode_data in episodes:
         response = create_draft_episode_in_transistor(episode_data)
         if response.status_code == 201:
             print(f"Draft episode '{episode_data['episode[title]']}' created successfully.")
+            
+            # Get the episode ID from the response
+            episode_id = response.json().get('id')
+            
+            # Retrieve the argument date from the CSV file
+            argument_date_match = re.search(r'\[Arg:\s(\d+\.\d+\.\d+)\]', episode_data['episode[title]'])
+            if argument_date_match:
+                argument_date_str = argument_date_match.group(1)
+                # Adjust the date format to match the input format (MM.DD.YYYY)
+                argument_date = datetime.strptime(argument_date_str, '%m.%d.%Y')
+                # Set the time to 22:00 UTC
+                argument_date = argument_date.replace(hour=22, minute=0, second=0)
+                # Format as ISO 8601
+                published_date = argument_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+            
+                # Update the episode with the publish date
+                update_response = update_episode_publish_date(episode_id, published_date)
+                if update_response.status_code == 200:
+                    print(f"Publish date updated for episode '{episode_data['episode[title]']}'")
+                else:
+                    print(f"Failed to update publish date for episode '{episode_data['episode[title]']}'")
+            else:
+                print(f"Argument date not found for episode '{episode_data['episode[title]']}'")
         else:
             print(f"Failed to create episode '{episode_data['episode[title]']}'. Status Code: {response.status_code}, Response: {response.json()}")
-
-
 
 process_episodes(csv_file_path)

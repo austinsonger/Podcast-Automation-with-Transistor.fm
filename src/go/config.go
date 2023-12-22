0e23c8afc4672b49a557b308dbc4357b135a54ec
Austin Songer <a.songer@protonmail.com>
2023-12-22 13:17:00 -0600

    Update config.go

--- src/go/config.go
+++ src/go/config.go
@@ -4,8 +4,11 @@ package config
 const (
     ApiURL      = "https://api.transistor.fm/v1/episodes"
     ShowID      = "12890"
+    imgurAPIURL = "https://api.imgur.com/3/upload"
+    caseBasePath = ".../2023/"
     CSVFilePath = "./config/go/scotus.csv"
 )
+
 
 // TransistorAPIKey retrieves the API Key from environment variables.
 func TransistorAPIKey() string {

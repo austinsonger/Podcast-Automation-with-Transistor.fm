6c2a445e6db2968c173205749587522426be5e18
Austin Songer <a.songer@protonmail.com>
2023-12-22 12:58:25 -0600

    Create episode-update-publish.go

---
+++ src/go/episode-update-publish.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "bufio"
+    "encoding/csv"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "time"
+)
+
+func publishEpisode(episodeID, apiURL, apiKey string) (json.RawMessage, error) {
+    url := fmt.Sprintf("%s/%s/publish", apiURL, episodeID)
+
+    client := &http.Client{}
+    req, err := http.NewRequest("POST", url, nil)
+    if err != nil {
+        return nil, err
+    }
+    req.Header.Set("x-api-key", apiKey)
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    var response json.RawMessage
+    err = json.NewDecoder(resp.Body).Decode(&response)
+    return response, err
+}
+
+func processCSV(csvPath, apiURL, apiKey string) error {
+    file, err := os.Open(csvPath)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    reader := csv.NewReader(bufio.NewReader(file))
+    rows, err := reader.ReadAll()
+    if err != nil {
+        return err
+    }
+
+    for _, row := range rows[1:] {
+        argumentDate, err := time.Parse("2006-01-02", row[1]) // Assuming 'Argument Date' is the second column
+        if err != nil {
+            fmt.Println("Error parsing date:", err)
+            continue
+        }
+
+        if row[2] != "" && argumentDate.Before(time.Now()) && row[3] != "yes" { // Assuming 'Published' is the fourth column
+            response, err := publishEpisode(row[2], apiURL, apiKey) // Assuming 'Episode ID' is the third column
+            if err != nil {
+                fmt.Println("Error publishing episode:", err)
+                continue
+            }
+            fmt.Printf("Episode %s published: %s\n", row[2], string(response))
+        }
+    }
+    return nil
+}
+
+func main() {
+    csvFilePath := "./config/scotus.csv"
+    transistorAPIURL := "https://api.transistor.fm/v1/episodes"
+    transistorAPIKey := os.Getenv("TRANSISTOR_API_KEY")
+
+    if err := processCSV(csvFilePath, transistorAPIURL, transistorAPIKey); err != nil {
+        fmt.Println("Error processing CSV file:", err)
+    }
+}
